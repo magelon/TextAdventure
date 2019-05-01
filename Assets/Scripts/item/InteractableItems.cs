060ee4f90fcc2430daaaf194e0f28411ae7f28c3
lon <alfardproject@gmail.com>
2019-05-01 14:14:29 -0700

    c

--- Assets/Scripts/item/InteractableItems.cs
+++ Assets/Scripts/item/InteractableItems.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class InteractableItems : MonoBehaviour
 {
     public Dictionary<string, string> examineDictionary = new Dictionary<string, string>();
+    public Dictionary<string, string> takeDictionary = new Dictionary<string, string>();
+
     [HideInInspector] public List<string> nounsInRoom = new List<string>();
     List<string> nounsInInventory = new List<string>();
 
-  public string GetObjectsNotInInventory(Room currentRoom,int i)
+    GameController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<GameController>();
+    }
+
+    public string GetObjectsNotInInventory(Room currentRoom,int i)
     {
         InteractableObject interactableInRoom = currentRoom.interactableObjectsInRoom[i];
         if (!nounsInInventory.Contains(interactableInRoom.noun))
@@ -23,5 +32,22 @@ public class InteractableItems : MonoBehaviour
     {
         examineDictionary.Clear();
         nounsInRoom.Clear();
+        takeDictionary.Clear();
+    }
+
+    public Dictionary<string,string> Take(string[] separatedInputWords)
+    {
+        string noun = separatedInputWords[1];
+
+        if (nounsInRoom.Contains(noun)){
+            nounsInInventory.Add(noun);
+            nounsInRoom.Remove(noun);
+            return takeDictionary;
+        }
+        else
+        {
+            controller.LogStringWithReturn("there is no " + noun + "here to take");
+            return null;
+        }
     }
 }
